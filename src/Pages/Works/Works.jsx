c83149c13a3ce3c601255a9c25d5c382b3d3d2da
mdhasankhan-123 <hasankha1023@gmail.com>
2023-11-06 14:31:53 +0600

    (PCS): Tabs components added in Works page

--- src/Pages/Works/Works.jsx
+++ src/Pages/Works/Works.jsx
@@ -1,9 +1,13 @@
 import PrimaryTitle from "../../Components/PrimaryTitle/PrimaryTitle";
+import Tabs from "../../Components/Tabs/Tabs";
 
 const Works = () => {
     return (
-        <div>
+        <div className="bg-[#1F1F1F] h-[100vh]">
             <PrimaryTitle first={'My'} last={'Portfolio'}></PrimaryTitle>
+            <div>
+                <Tabs></Tabs>
+            </div>
         </div>
     );
 };
