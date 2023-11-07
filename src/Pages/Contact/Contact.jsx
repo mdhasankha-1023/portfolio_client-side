9a1c6c41c4b449f6d4434dabf87accdf6a0aa81d
mdhasankhan-123 <hasankha1023@gmail.com>
2023-11-08 03:32:04 +0600

    (PCS): Responsive code added on Contact page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -8,8 +8,8 @@ const Contact = () => {
     return (
         <div className="bg-[#1F1F1F] h-[100%]">
             <PrimaryTitle first={'Get in'} last={'Touch'}></PrimaryTitle>
-            <div className="flex gap-8 z-90">
-                <div className="w-[40%] p-4">
+            <div className="flex flex-col lg:flex-row gap-8 z-90">
+                <div className="w-full lg:w-[40%] p-4">
                     <h1 className="text-white text-4xl text-start uppercase">Don't be Shy!</h1>
                     <p className="text-white text-lg text-justify my-6">Feel free to get in touch with me. I am always open to discussing new projects, creative ideas or opportunities to be part of your visions.</p>
                     <div className="flex gap-8 flex-col text-white">
@@ -42,7 +42,7 @@ const Contact = () => {
                         </div>
                     </div>
                 </div>
-                <div className="w-[60%] p-4">
+                <div className="w-full lg:w-[60%] p-4">
                     <ContactForm></ContactForm>
                 </div>
             </div>
