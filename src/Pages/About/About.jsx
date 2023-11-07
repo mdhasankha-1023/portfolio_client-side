bfa0fc2626d85dfc1ba97bad2efe72ea5b8aaa4f
mdhasankhan-123 <hasankha1023@gmail.com>
2023-11-08 01:11:24 +0600

    (PCS): Introduce text style fixed of About Me page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -14,48 +14,60 @@ const About = () => {
         <div className="bg-[#1F1F1F] h-[200%]">
             <PrimaryTitle first={'About'} last={'Me'}></PrimaryTitle>
             <div className="flex text-white">
-                <div className="w-[35%] px-4">
+                <div className="w-[40%] px-4">
                     <h1 className="text-stat text-4xl font-bold mb-10">Personal Infos</h1>
-                    <div className="grid grid-cols-2 gap-8">
+                    <div className="grid grid-cols-2 gap-12 overflow-hidden">
+                        {/* name */}
                         <div className="flex flex-col gap-1">
-                            <h5>Full Name:</h5>
-                            <p>MD. Hasan Kha</p>
+                            <span className="text-xl text-gray-400">Full Name:</span>
+                            <p className='font-bold text-xl text-gray-300'>MD. Hasan Kha</p>
                         </div>
-                        <div>
-                            <h5>Nationality:</h5>
-                            <p>Bangladeshi</p>
+
+                        {/* nationality */}
+                        <div className="flex flex-col gap-1">
+                            <span className="text-xl text-gray-400">Nationality:</span>
+                            <p className='font-bold text-xl text-gray-300'>Bangladeshi</p>
                         </div>
-                        <div>
-                            <h5>Address:</h5>
-                            <p>Bangladesh</p>
+
+                        {/* address */}
+                        <div className="flex flex-col gap-1">
+                            <span className="text-xl text-gray-400">Address:</span>
+                            <p className='font-bold text-xl text-gray-300'>Dhaka, Bangladesh</p>
                         </div>
-                        <div>
-                            <h5>Phone:</h5>
-                            <p>+880 1646448338</p>
+
+                        {/* phone */}
+                        <div className="flex flex-col gap-1">
+                            <span className="text-xl text-gray-400">Phone:</span>
+                            <p className='font-bold text-xl text-gray-300'>+880 1646448338</p>
                         </div>
-                        <div>
-                            <h5>Email:</h5>
-                            <p>mdhasankha.wd@gmail.com</p>
+
+                        {/* email */}
+                        <div className="flex flex-col gap-1">
+                            <span className="text-xl text-gray-400">Email:</span>
+                            <p className='font-bold text-xl text-gray-300 text-clip'>mdhasankha.wd@gmail.com</p>
                         </div>
-                        <div>
-                            <h5>Language:</h5>
-                            <p>English, Bangla</p>
+
+                        {/* language */}
+                        <div className="flex flex-col gap-1">
+                            <span className="text-xl text-gray-400">language:</span>
+                            <p className='font-bold text-xl text-gray-300'>Bangla, English</p>
                         </div>
+
                     </div>
                     <div className="flex justify-start items-center mt-16">
-                        <a  href={resume}>
+                        <a href={resume}>
                             <PrimaryBtn text={'Download Resume'} icon={faDownload}>
                             </PrimaryBtn>
                         </a>
 
                     </div>
                 </div>
-                <div className="w-[65%]">
-                    <p className="text-justify p-4">I'm a Tunisian based web designer & front‑end developer focused on crafting clean & user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me. I'm a Tunisian based web designer & front‑end developer focused on crafting clean & user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me.</p>
+                <div className="w-[60%]">
+                    <p className="text-justify pr-24 leading-8 text-xl tracking-wider">I'm a Tunisian based web designer & front‑end developer focused on crafting clean & user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me. I'm a Tunisian based web designer & front‑end developer focused on crafting clean & user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me.I'm a Tunisian based web designer & front‑end developer focused on crafting clean & user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me.</p>
                 </div>
             </div>
             <div className="py-20 text-white">
-                <h1 className="text-center text-3xl uppercase">My Skills</h1>
+                <h1 className="text-center text-4xl uppercase">My Skills</h1>
                 <div className="w-[50%] mx-auto my-10">
                     <Skills></Skills>
                 </div>
