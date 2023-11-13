fb72c658cb64b002c5e0b7bac97bbe42e363838d
mdhasankha-1023 <mdhasankha.wd@gmail.com>
2023-11-13 18:25:26 +0600

    (PCS): Changed social media link

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -6,9 +6,9 @@ const Skills = () => {
     return (
         <div className='flex flex-col gap-6 font-bold text-xl'>
             <SkillBar name="HTML 5" level={70} color="#E44D25" />
-            <SkillBar name="CSS 3" level={60} color="#337CC5" />
+            <SkillBar name="CSS 3" level={70} color="#337CC5" />
             <SkillBar name="JavaScript" level={50} color="#FFD500" />
-            <SkillBar name="React.js" level={70} color="#53C1DE" />
+            <SkillBar name="React.js" level={60} color="#53C1DE" />
             <SkillBar name="Node.js" level={30} color="#AED373" />
             <SkillBar name="Express.js" level={30} color="#868686" />
             <SkillBar name="MongoDB" level={30} color="#499D4A" />
